7440c1444367ed3c36b113185ed8db4f7b7399e4
AdiriOghenetega <tegaadiri3@yahoo.com>
2023-06-27 16:24:40 +0100

    updated

--- controllers/userctrl.js
+++ controllers/userctrl.js
@@ -43,7 +43,7 @@ const handleSignUp = async (req, res) => {
 const handleMobileSignUp = async (req, res) => {
   console.log("handle mobile signup called")
   console.log(req.body)
-  console.log(req.file)
+  console.log(req)
   const { email, firstName, lastName, image, address,mobile } = req.body;
   try {
     const userExist = await userModel.findOne({ email: email });
